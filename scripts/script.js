5919ead3447136b4239b8c6f5b5cee2a1661f841
ulasuregen <61901077+ulasuregen@users.noreply.github.com>
2025-08-23 16:05:56 +0300

    I've solved the size of the squares in CSS

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,7 @@
 const container = document.querySelector('.container');
 
-for(i = 0; i < 16; i++){
+for(i = 0; i < 16 * 16; i++){
     const sub_container = document.createElement('div');
     sub_container.classList.add('sub-container');
-    
-    let random_color = [Math.random() * 255,Math.random() * 255,Math.random() * 255];
-    sub_container.style.backgroundColor = 'rgba(' + random_color.join(',') + ')';
     container.appendChild(sub_container);
 }
